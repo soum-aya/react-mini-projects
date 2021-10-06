07fa47b5b8fc325807d7c363ab88dc254240b943
Soumaya <soumaya.assadiki@gmail.com>
2021-10-06 16:28:11 +0100

    add sidebar-modal mini-project

---
+++ sidebar-modal/src/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+
+const Home = () => {
+  // const data = useContext(AppContext);
+  // const data = useGlobalContext(); //using custom hook
+  const { openSidebar, openModal } = useGlobalContext();
+
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button className="btn" onClick={openModal}>
+        show modal
+      </button>
+    </main>
+  );
+};
+
+export default Home;
