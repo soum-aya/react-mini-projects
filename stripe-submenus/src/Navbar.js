c8a5b9fc5deaa8af6106c2c4c850bbc7cdb4e968
Soumaya <soumaya.assadiki@gmail.com>
2021-10-07 22:56:31 +0100

    add navbar in stripe-submenus mini-project

---
+++ stripe-submenus/src/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+
+const Navbar = () => {
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  const displaySubmenu = (e) => {
+    console.log("hi there!");
+    openSubmenu();
+  };
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="stripe" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              products
+            </button>
+          </li>
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              developers
+            </button>
+          </li>
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              company
+            </button>
+          </li>
+        </ul>
+        <button className="btn signin-btn" onMouseOver={displaySubmenu}>
+          sign in
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
