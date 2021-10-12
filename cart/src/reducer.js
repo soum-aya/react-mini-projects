60c880b7200e9798ea8bd36c79b31c373456307d
Soumaya <soumaya.assadiki@gmail.com>
2021-10-12 18:42:19 +0100

    add cart mini-project

---
+++ cart/src/reducer.js
@@ -0,0 +1,29 @@
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+  if (action.type === "REMOVE_ITEM") {
+    const newCart = state.cart.filter((cartItem) => cartItem.id !== action.payload);
+    return { ...state, cart: newCart };
+  }
+  if (action.type === "INCREASE") {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === "DECREASE") {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount - 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
+  }
+  return state;
+};
+export default reducer;
