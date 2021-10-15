12cd9ff3ffe363c0d727079940cf58157b51f521
Soumaya <soumaya.assadiki@gmail.com>
2021-10-15 17:22:51 +0100

    add cocktails mini-project

---
+++ cocktails/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse condimentum augue at purus molestie tincidunt. Nam euismod ipsum id nisl mollis, sit amet pharetra massa convallis. Nam consectetur, sapien et viverra eleifend, arcu nunc semper metus, sed euismod est justo.</p>
+    </section>
+  );
+};
+
+export default About;
