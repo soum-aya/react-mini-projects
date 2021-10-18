f8a326b44a5920f8c0aa662c4bdcb21b14396af5
Soumaya <soumaya.assadiki@gmail.com>
2021-10-18 22:33:35 +0100

    add single cocktail page in cocktails mini-project

--- cocktails/src/components/SearchForm.js
+++ cocktails/src/components/SearchForm.js
@@ -12,10 +12,13 @@ const SearchForm = () => {
   const searchCocktail = () => {
     setSearchTerm(searchValue.current.value);
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <section className="section search">
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">search your favourite cocktail</label>
           <input type="text" id="name" ref={searchValue} onChange={searchCocktail} />
